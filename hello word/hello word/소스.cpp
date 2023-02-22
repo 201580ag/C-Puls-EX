18d7b3b22efc15d5d477ac57c32b85c6163963fd
201580ag <39054577+201580ag@users.noreply.github.com>
2023-02-22 20:00:19 +0900

    Add files via upload

---
+++ hello word/hello word/소스.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <conio.h>
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "계속하려면 아무 키나 누르십시오..." << std::endl;
+    _getch(); // conio.h 라이브러리의 함수
+    return 0;
+}
