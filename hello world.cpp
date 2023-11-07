0d9541cdddc2dc899f5740af8dd2af6861bcd7cf
201580ag <39054577+201580ag@users.noreply.github.com>
2023-11-07 13:30:39 +0900

    Create hello world.cpp

---
+++ hello world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "계속하려면 아무 키나 누르십시오..." << endl;
+    _getch();
+    return 0;
+}
